151f8610edb983442bb0e42558453a8461b1a10b
walter <walter.stocker@gmx.de>
2018-06-16 23:14:04 +0200

    chore: styling of blog entry

--- src/components/pages/blog-entry-content.js
+++ src/components/pages/blog-entry-content.js
@@ -3,10 +3,20 @@ import React from 'react'
 import showdown from 'showdown'
 import prettify from 'showdown-prettify' // eslint-disable-line no-unused-vars
 
-const converter = new showdown.Converter({ extensions: ['prettify'] })
+const converter = new showdown.Converter({
+  headerLevelStart: 3,
+  extensions: ['prettify']
+})
+converter.setFlavor('github')
+
+const BlogEntryContentStyle = {
+  maxHeight: '10em',
+  overflow: 'hidden'
+}
 
 const BlogEntryContent = content => (
   <div
+    style={BlogEntryContentStyle}
     dangerouslySetInnerHTML={{
       __html: converter.makeHtml(content)
     }}
